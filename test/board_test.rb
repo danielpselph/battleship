6c8521eb3cfbd7a4bcc6d5dd041e76c23be53c97
Daniel Selph <daniel.p.selph@gmail.com>
2019-12-06 12:28:47 -0700

    Add valid cell placement tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,6 +8,8 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_board_exists
@@ -20,5 +22,27 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.validate_coordinate?("A5")
     assert_equal false, @board.validate_coordinate?("E1")
     assert_equal false, @board.validate_coordinate?("A22")
+  end
+
+  def test_valid_placement_length
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_valid_placement_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+  end
+
+  def test_valid_placement_not_diagonal
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  def test_valid_placement
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 end
