7205dbebf08cc4ebd1fc83ad0bce54983e2b0504
Jordan Sewell <jrsewell400@gmail.com>
2019-12-09 17:34:19 -0700

    Finish writing individual methods and now working on if statement within valid_placement

--- lib/board.rb
+++ lib/board.rb
@@ -36,24 +36,57 @@ class Board
       y << (val.split(//)[1]).to_i
     end
 
-    y.each_cons(2).all? do |x, y|
-      if x == y - 1
-        true
-      else
-        false
-      end
-    end
 
-    x.each_cons(2).all? do |x, y|
-      if x.ord == y.ord - 1
-        true
-      else
-        false
+    if valid_x(x) == true && valid_y(y) == true
+      false
+    elsif hor_x(x) == true && valid_y(y) == true || valid_x(x) == true && vert_y(y) == true
+      true
+    elsif
+      false
+    end
+    # valid_y(y)
+    # valid_x(x)
+    # vert_y(y)
+    # hor_x(x)
+
+  end
+
+  def valid_y(y)
+      y.each_cons(2).all? do |x, y|
+        if x == y - 1
+          true
+        else
+          false
+        end
       end
+  end
+
+  def valid_x(x)
+      x.each_cons(2).all? do |x, y|
+        if x.ord == y.ord - 1
+          true
+        else
+          false
+        end
+      end
+  end
+
+  def hor_x(x)
+    if x.each {|let| let.ord == let.ord}
+      true
+    else
+      false
+    end
+  end
+
+  def vert_y(y)
+    if y.each {|num| num == num}
+      true
+    else
+      false
     end
   end
 end
-
 #BUILD y.each AND x.each INTO SEPARATE METHODS, AND CALL THEM IN .valid_placement?
 #   def validate_y
 #     y.each_cons(2) do |num|
